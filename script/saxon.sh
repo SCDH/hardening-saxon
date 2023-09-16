eb4174d716bf57fdc7ceb827205b37e1d1f6bc3f
Christian Lück <christian.lueck@uni-muenster.de>
2023-09-17 01:51:30 +0200

    wrapper scripts

---
+++ script/saxon.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+JARS=${project.build.directory}/${project.artifactId}-${project.version}.jar
+JARS=$JARS:${project.build.directory}/lib/Saxon-HE-${saxon.version}.jar
+JARS=$JARS:${project.build.directory}/lib/xmlresolver-${xmlresolver.version}.jar
+
+java $JAVAOPTS -cp $JARS $@
